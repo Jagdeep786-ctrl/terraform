3da7a6bb50c805e4de0cffd863fead831a3c96ea
Jagdeep786-ctrl <jagdeepsodhi144@gmail.com>
2026-03-14 02:09:07 -0600

    terraform file updated for prd

--- output.tf
+++ output.tf
@@ -1,6 +1,9 @@
 output "ec2_public_ip" {
-  value = [for key in aws_instance.my-instance: key.public_ip]
+    value = [for key in aws_instance.my-instance: key.public_ip]
+    description = "Public IP address of the EC2 instance"
+  
 }
+
 output "ec2_private_ip" {
     value = [for key in aws_instance.my-instance: key.private_ip]
     description = "Private IP address of the EC2 instance"
